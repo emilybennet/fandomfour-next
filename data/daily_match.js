3bae3ebe7192a9ee5dd06ab993d6efcedc19b377
Emily Bennet <me@emilybennet.com>
2019-03-07 22:16:30 -0500

    set poll launch day

--- data/daily_match.js
+++ data/daily_match.js
@@ -3,7 +3,7 @@ export default [
     playerA: "7×13",
     playerB: "4×9",
     voteUrl: null,
-    voteTime: null,
+    voteTime: "3/11 Noon",
     meta: false,
     id: "sun-r1-m1"
   },
@@ -11,7 +11,7 @@ export default [
     playerA: "5×12",
     playerB: "5×7",
     voteUrl: null,
-    voteTime: null,
+    voteTime: "3/11 6pm",
     meta: false,
     id: "stars-r1-m3"
   }
