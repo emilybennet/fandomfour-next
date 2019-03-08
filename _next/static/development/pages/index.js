349179945ecdf993e969e132305ea8553fc386ec
Emily Bennet <me@emilybennet.com>
2019-03-07 22:17:12 -0500

    deploy gh-pages

--- _next/static/development/pages/index.js
+++ _next/static/development/pages/index.js
@@ -1960,14 +1960,14 @@ __webpack_require__.r(__webpack_exports__);
   playerA: "7×13",
   playerB: "4×9",
   voteUrl: null,
-  voteTime: null,
+  voteTime: "3/11 Noon",
   meta: false,
   id: "sun-r1-m1"
 }, {
   playerA: "5×12",
   playerB: "5×7",
   voteUrl: null,
-  voteTime: null,
+  voteTime: "3/11 6pm",
   meta: false,
   id: "stars-r1-m3"
 }]);
