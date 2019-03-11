ff9fcbf945d57da26dcd78012cbf095d0d73d243
Emily Bennet <me@emilybennet.com>
2019-03-11 18:58:41 -0400

    less copy pasta.

--- data/daily_match.js
+++ data/daily_match.js
@@ -1,18 +1,7 @@
-export default [
-  {
-    playerA: "7×13",
-    playerB: "4×9",
-    voteUrl: "https://twitter.com/BronyCon/status/1105136309033672704",
-    voteTime: "3/11 Noon",
-    meta: false,
-    id: "sun-r1-m1"
-  },
-  {
-    playerA: "5×12",
-    playerB: "5×7",
-    voteUrl: "https://twitter.com/BronyCon/status/1105226903072452609",
-    voteTime: "6pm",
-    meta: false,
-    id: "stars-r1-m3"
-  }
-];
+import sun from "./conference_sun";
+import moon from "./conference_moon";
+import stars from "./conference_stars";
+import chaos from "./conference_chaos";
+import finals from "./conference_finals";
+
+export default [moon.round1[1], chaos.round1[3]];
