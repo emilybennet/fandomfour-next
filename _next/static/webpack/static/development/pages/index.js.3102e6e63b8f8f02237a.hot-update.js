36da500b0a121ad22e43efb54fb439094ccdc8e3
Emily Bennet <me@emilybennet.com>
2019-03-30 20:23:18 -0400

    deploy gh-pages

---
+++ _next/static/webpack/static/development/pages/index.js.3102e6e63b8f8f02237a.hot-update.js
@@ -0,0 +1,90 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Champion/index.js":
+/*!**************************************!*\
+  !*** ./components/Champion/index.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _Brust__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Brust */ "./components/Champion/Brust.js");
+/* harmony import */ var _Title__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../Title */ "./components/Title.js");
+/* harmony import */ var _data_players__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../data/players */ "./data/players.js");
+
+var _jsxFileName = "/Users/ebennet/Projects/BronyCon/Fandom Four 2019/fandomfour-next/components/Champion/index.js";
+
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\ngrid-column: 1 / auto;\n    grid-row: 1 / auto;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+
+
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_2__["default"].section.withConfig({
+  displayName: "Champion__Container",
+  componentId: "vbtpxd-0"
+})(["align-items:center;background:url(\"/static/karen-pattern.webp\") repeat,#573484;clip-path:polygon(0 0,100% 0,100% 100%,0 calc(100% - 4vw));display:grid;grid-template-columns:1fr;grid-template-rows:1fr;justify-content:center;max-height:800px;overflow:hidden;padding:200px 15px;position:relative;z-index:400;.no-webp &{background:url(\"/static/karen-pattern.png\") repeat,#573484;}svg{grid-column:1 / auto;grid-row:1 / auto;height:1000px;mix-blend-mode:overlay;opacity:0.5;}polygon{fill:white;}"]);
+var WinnerIntel = style.div(_templateObject());
+var Matches = styled_components__WEBPACK_IMPORTED_MODULE_2__["default"].div.withConfig({
+  displayName: "Champion__Matches",
+  componentId: "vbtpxd-1"
+})(["align-items:center;display:flex;flex-direction:row;justify-content:center;@media (max-width:973px){flex-direction:column;}"]);
+var TitleYellow = Object(styled_components__WEBPACK_IMPORTED_MODULE_2__["default"])(_Title__WEBPACK_IMPORTED_MODULE_4__["default"]).withConfig({
+  displayName: "Champion__TitleYellow",
+  componentId: "vbtpxd-2"
+})(["color:", ";"], function (props) {
+  return props.theme.yellow;
+});
+var WinnerName = styled_components__WEBPACK_IMPORTED_MODULE_2__["default"].h2.withConfig({
+  displayName: "Champion__WinnerName",
+  componentId: "vbtpxd-3"
+})([""]);
+
+var DailyMatch = function DailyMatch(props) {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(Container, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Brust__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(WinnerName, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }, "Slice of Life")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (DailyMatch);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.3102e6e63b8f8f02237a.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.a7ca334919b0fde6c8b7.hot-update.js
@@ -0,0 +1,90 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Champion/index.js":
+/*!**************************************!*\
+  !*** ./components/Champion/index.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _Brust__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Brust */ "./components/Champion/Brust.js");
+/* harmony import */ var _Title__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../Title */ "./components/Title.js");
+/* harmony import */ var _data_players__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../data/players */ "./data/players.js");
+var _jsxFileName = "/Users/ebennet/Projects/BronyCon/Fandom Four 2019/fandomfour-next/components/Champion/index.js";
+
+
+
+
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].section.withConfig({
+  displayName: "Champion__Container",
+  componentId: "vbtpxd-0"
+})(["align-content:center;align-items:center;background:url(\"/static/karen-pattern.webp\") repeat,#573484;clip-path:polygon(0 0,100% 0,100% 100%,0 calc(100% - 4vw));display:grid;font-size:1.5em;grid-template-columns:1fr;grid-template-rows:1fr;justify-items:center;max-height:800px;overflow:hidden;padding:200px 15px;position:relative;text-align:center;z-index:400;.no-webp &{background:url(\"/static/karen-pattern.png\") repeat,#573484;}svg{grid-column:1 / auto;grid-row:1 / auto;height:1000px;mix-blend-mode:overlay;opacity:0.5;}polygon{fill:white;}"]);
+var WinnerIntel = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "Champion__WinnerIntel",
+  componentId: "vbtpxd-1"
+})(["grid-column:1 / auto;grid-row:1 / auto;transform:rotate(4deg);"]);
+var WinnerName = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].span.withConfig({
+  displayName: "Champion__WinnerName",
+  componentId: "vbtpxd-2"
+})(["color:", ";display:block;font-size:3em;font-style:italic;font-weight:600;"], function (props) {
+  return props.theme.yellow;
+});
+var YellowText = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].span.withConfig({
+  displayName: "Champion__YellowText",
+  componentId: "vbtpxd-3"
+})(["color:", ";display:block;font-weight:400;"], function (props) {
+  return props.theme.white;
+});
+
+var DailyMatch = function DailyMatch(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Container, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 60
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Brust__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WinnerIntel, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(YellowText, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, "Congratulations"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WinnerName, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 67
+    },
+    __self: this
+  }, "Slice of Life"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(YellowText, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "2019 Grand Champion")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (DailyMatch);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.a7ca334919b0fde6c8b7.hot-update.js.map
